2bd70e5650840cc76033ff1fb288afd4b7aed5b5
Yurij <info@rightblog.ru>
2018-06-26 16:43:15 +0300

    check

--- js/login-form-check.js
+++ js/login-form-check.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
 // init modul
 	var loginFormCheck = (function () {
+		
 		// Переменные модуля
 		var _form = $('#form-login');
-
 		var _email = $('#email-fild'); //поле email
+
 		var _password = $('#password-fild'); //поле pass
 
 		var _error = $('<div class="error"></div>'); 
@@ -28,7 +29,11 @@ $(document).ready(function() {
 		// Приватные методы
 
 		var _formValidate = function (event) {
+
+			console.log('_formValidate - start');
+
 			$('div.error-description').remove();
+
 			var	noEmail = _email.attr('data-empty'),
 				emailFildEmpty = _email.attr('data-empty'),
 				noPassword = _password.attr('data-empty'),
@@ -37,51 +42,67 @@ $(document).ready(function() {
 			var userPassword = '123',
 				userEmail = 'mail@mail.com';
 
-	    		event.preventDefault();
-	    		if (_password.val() == '') {
-	    			var passwordFildEmpty = _error.clone().text(noPassword);
-	    			_password.before(passwordFildEmpty);
-	    			passwordFildEmpty.fadeIn(800);
-	    			_password.on('focus', function() {
+				event.preventDefault();
+
+				var passwordCheck = true,
+					emailCheck = true;
+
+				// Если пустой пароль
+				if (_password.val() == '') {
+					console.log('Password is empty');
+					var passwordFildEmpty = _error.clone().text(noPassword);
+					_password.before(passwordFildEmpty);
+					passwordFildEmpty.fadeIn(800);
+					_password.on('focus', function() {
 						passwordFildEmpty.fadeOut(500);
-					});	    			
-	    		}
-	    		if ( _email.val() == "" ) {
-	    			var emailFildEmpty = _error.clone().text(noEmail);
+					});	   
+					passwordCheck = false;
+				}
+
+				// Если пустой  email
+				if ( _email.val() == "" ) {
+					console.log('Email is empty');
+					var emailFildEmpty = _error.clone().text(noEmail);
 					_email.before(emailFildEmpty);
 					emailFildEmpty.fadeIn(800);
 					_email.on('focus', function() {
 						emailFildEmpty.fadeOut(500);
 					});
+					emailCheck = false;
+				} 
+
+				// Проверка на формат  email
+				if ( emailCheck == true && !(pattern.test( _email.val() )) ) {
+					var errorDataFormat = _error.clone().text(DataFormat);
+					_email.before(errorDataFormat);
+					errorDataFormat.fadeIn(800);
+					emailCheck = false;
 				}
-				else {
-					if (!(pattern.test(_email.val()))) {
-						var errorDataFormat = _error.clone().text(DataFormat);
+
+				// Если пароль не пустой и email верного формата  - проверяем на корректные доступы
+				if (emailCheck == true && passwordCheck == true) {
+					if (_password.val().trim() == userPassword && _email.val() == userEmail) {
+						console.log('Credentails are correct');
+						_form.unbind('submit').submit();
+						_error.fadeOut;
+					} else {
+						console.log('Credentails NOT correct');
+
+						var errorDataFormat = _error.clone().text(dataErrorUnknown);
+						var emailDescription = '<div class="error-description"><p>Введите верные данные для входа или воспользуйтесь <a href="#">восстановлением пароля, </a>чтобы войти на сайт.</p></div>';
+						_email.before(emailDescription);
 						_email.before(errorDataFormat);
 						errorDataFormat.fadeIn(800);
+						_email.on('focus', function() {
+							$('div.error-description').fadeOut(800);
+							errorDataFormat.fadeOut(800);
+						});
 					}
-					else {
-						if (_password.val().trim() == userPassword && _email.val() == userEmail) {
-							_form.unbind('submit').submit();
-							_error.fadeOut;
-						}
-						else {
-							var errorDataFormat = _error.clone().text(dataErrorUnknown);
-							var emailDescription = '<div class="error-description"><p>Введите верные данные для входа или воспользуйтесь <a href="#">восстановлением пароля, </a>чтобы войти на сайт.</p></div>';
-							_email.before(emailDescription);
-							_email.before(errorDataFormat);
-							errorDataFormat.fadeIn(800);
-							_email.on('focus', function() {
-								$('div.error-description').fadeOut(800);
-								errorDataFormat.fadeOut(800);
-							});
-						}
-					}	
 				}
+
 			}
-		return {
-			init
-		}
+
+		return { init }
 	}());
 	loginFormCheck.init();
 
