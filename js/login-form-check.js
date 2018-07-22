c26f96cb2b6e330af4c5057cc2ed88cc84a42d99
Sergey <verstka.sv@mail.rugit config --global user.email verstka.sv@mailgit config --global user.email verstka.sv@mail.ru>
2018-07-22 14:52:30 +0300

    fix errors

--- js/login-form-check.js
+++ js/login-form-check.js
@@ -28,53 +28,80 @@ $(document).ready(function() {
 		// Приватные методы
 
 		var _formValidate = function (event) {
-			var	noEmail = _email.attr('data-empty'),
-				emailFildEmpty = _email.attr('data-empty'),
-				noPassword = _password.attr('data-empty'),
-				DataFormat = _password.attr('data-error-format'),
-				dataErrorUnknown = _email.attr('data-error-unknown');
-			var userPassword = '123',
-				userEmail = 'mail@mail.com';
+    		event.preventDefault();
+    		var userPassword = '123',
+    			userEmail = 'mail@mail.com';
 
-	    		event.preventDefault();
+	    	var passwordFildEmpty = _error.clone().text(_password.attr('data-empty'));
+	    	var emailFildEmpty = _error.clone().text(_email.attr('data-empty'));
+			var errorDataFormat = _error.clone().text(_password.attr('data-error-format'));
+			var errorDataFormatEmail = _error.clone().text(_email.attr('data-error-format'));
+
+			$('div.error-description').remove();
+			var error = {
+				noEmail:_email.attr('data-empty'),
+				emailFildEmpty:_email.attr('data-empty'),
+				noPassword:_password.attr('data-empty'),
+				DataFormat:_email.attr('data-error-format'),
+				dataErrorUnknown:_email.attr('data-error-unknown'),
+				val:function(err) {
+					return _error.clone().text(err);
+				}
+			}
+			
+    		var fildEmpty = function() {
+    			var result = false;
 	    		if (_password.val() == '') {
-	    			var passwordFildEmpty = _error.clone().text(noPassword);
+	    			var passwordFildEmpty = error.val(error.noPassword);
 	    			_password.before(passwordFildEmpty);
 	    			passwordFildEmpty.fadeIn(800);
 	    			_password.on('focus', function() {
 						passwordFildEmpty.fadeOut(500);
-					});	    			
+					});
+					result = true; 			
 	    		}
 	    		if ( _email.val() == "" ) {
-	    			var emailFildEmpty = _error.clone().text(noEmail);
+	    			var emailFildEmpty = error.val(error.noEmail);
 					_email.before(emailFildEmpty);
 					emailFildEmpty.fadeIn(800);
 					_email.on('focus', function() {
 						emailFildEmpty.fadeOut(500);
 					});
+					result = true; 			
 				}
-				else {
-					if (!(pattern.test(_email.val()))) {
-						var errorDataFormat = _error.clone().text(DataFormat);
-						_email.before(errorDataFormat);
-						errorDataFormat.fadeIn(800);
-					}
-					else {
-						if (_password.val().trim() == userPassword && _email.val() == userEmail) {
-							_form.unbind('submit').submit();
-							_error.fadeOut;
-						}
-						else {
-							var errorDataFormat = _error.clone().text(dataErrorUnknown);
-							_email.before(errorDataFormat);
-							errorDataFormat.fadeIn(800);
-						}
-					}	
-				}
-			}
+				return result;
+    		}
+    		//fildEmpty();
+    		if (fildEmpty() == false) {
+    			if (!(pattern.test(_email.val()))) {
+    				var errorDataFormat = error.val(error.DataFormat);
+    				_email.before(errorDataFormat);
+    				errorDataFormat.fadeIn(800);
+    			}
+    			else {
+    				if (_password.val().trim() == userPassword && _email.val() == userEmail) {
+    					_form.unbind('submit').submit();
+    					_error.fadeOut;
+    				}
+    				else {
+    					var errorDataFormat = error.val(error.dataErrorUnknown);
+    					var emailDescription = '<div class="error-description"><p>Введите верные данные для входа или воспользуйтесь <a href="#">восстановлением пароля, </a>чтобы войти на сайт.</p></div>';
+    					_email.before(emailDescription);
+    					_email.before(errorDataFormat);
+    					errorDataFormat.fadeIn(800);
+    					_email.on('focus', function() {
+    						$('div.error-description').fadeOut(800);
+    						errorDataFormat.fadeOut(800);
+    					});
+    				}
+    			}	
+    		}
+
+		}
 		return {
 			init
 		}
+			
 	}());
 	loginFormCheck.init();
 
